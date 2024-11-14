f51ce3c444c29aeeffc8bb90bd4cfa42851ac9e3
Massimiliano Fasi <m.fasi@leeds.ac.uk>
2024-11-14 10:54:17 +0000

    Fix bug and reduce matrix sizes in tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -34,12 +34,10 @@ int main() {
     std::cout << "Relative error: " << relErr << std::endl;
     assert(relErr < 1e-15);
 
-    return 0;
-
-    // Genereate a 5 x 3 matrix.
-    for (size_t m = 50; m <= 200; m += 50) {
-        for (size_t p = 50; p <= 200; p += 50) {
-            for (size_t n = 50; n <= 200; n += 50) {
+    // Test different sizes.
+    for (size_t m = 20; m <= 100; m += 20) {
+        for (size_t p = 20; p <= 100; p += 20) {
+            for (size_t n = 20; n <= 100; n += 20) {
                 std::vector<my_fp_type> A(m * p);
                 std::vector<my_fp_type> B(p * n);
 
@@ -48,14 +46,10 @@ int main() {
                 // Initalize matrix with random values between -10 and 10.
                 std::default_random_engine generator(std::random_device{}());
                 std::uniform_real_distribution<double> distribution(-100000.0, 100000.0);
-                for (size_t i = 0; i < m; i++)
-                    for (size_t j = 0; j < p; j++)
-                        A[i * m + j] = distribution(generator);
-                for (size_t i = 0; i < p; i++)
-                    for (size_t j = 0; j < n; j++)
-                        B[i * n + j] = distribution(generator);
-                A[0] = 1;
-
+                for (auto & element : A)
+                    element = distribution(generator);
+                for (auto & element : B)
+                    element = distribution(generator);
                 auto C = gemmi<my_fp_type, int8_t, int32_t>(A, B, m, p, n, 10);
                 auto C_ref = reference_gemm(A, B, m, p, n);
 
@@ -66,9 +60,6 @@ int main() {
             }
         }
     }
-
-    //std::cout << std::hex << std::bit_cast<my_int_type>(C[0]) << std::endl;
-    //std::cout << std::hex << std::bit_cast<my_int_type>(C_ref[0]) << std::endl;
 
     return 0;
 }
